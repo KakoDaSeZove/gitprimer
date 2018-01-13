c690c22e3f9187042dd9fd05a551f80e27980783
Your Name <you@example.com>
2018-01-13 20:22:28 +0100

    pogresno sam mergovala

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 public static void main (String [args]){
 
-System.out.println("Hello world pozdrav iz   grane3")
+System.out.println("Hello world pozdrav iz master grane")
 
 }
 }
